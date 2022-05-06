f6984b7d781c5796a453eb91f31163227865f79f
Jason Kerney <robert.kerney@edf-re.com>
2022-05-06 15:55:14 -0700

    F add new task

--- app/walrusbucket.ts
+++ app/walrusbucket.ts
@@ -1,9 +1,12 @@
 import { IContainer } from "@/container";
-import { ITask } from "./task";
+import { strictEqual } from "assert";
+import { Size, Unsized } from "./generalTypes";
+import { ITask, TaskConstructor } from "./task";
 
 export abstract class IWalrusBucket {
   abstract getAllTasks(): ITask[];
   abstract add(task: ITask): void;
+  abstract addNew(name: string, size?: Size): void;
 
   // abstract getCompleteTasks();
 
@@ -16,9 +19,11 @@ export type WalrusBucketConstructor = () => IWalrusBucket;
 
 class WalrusBucket extends IWalrusBucket {
   private tasks: ITask[] = [];
+  private taskBuilder : TaskConstructor;
 
-  constructor() {
+  constructor(taskBuilder: TaskConstructor) {
     super();
+    this.taskBuilder = taskBuilder;
   }
 
   getAllTasks(): ITask[] {
@@ -28,10 +33,14 @@ class WalrusBucket extends IWalrusBucket {
   add(task: ITask): void {
     this.tasks.push(task);
   }
+
+  addNew(name: string, size?: Size): void {
+    this.add(this.taskBuilder(name, size))
+  }
 }
 
 export function walrusBucketBuilder(factory: IContainer): WalrusBucketConstructor {
   return function (): IWalrusBucket {
-    return new WalrusBucket();
+    return new WalrusBucket(factory.build(ITask));
   };
 }
--- test/walrusbucket.spec.ts
+++ test/walrusbucket.spec.ts
@@ -5,38 +5,68 @@ import { ITask, TaskConstructor } from "@/task";
 import { TimeHelper } from "./helpers";
 
 describe('Walrus Bucket should', () => {
-    let container: IContainer;
-    let walrusBucketConstructor: WalrusBucketConstructor;
-    let taskConstructor: TaskConstructor;
-    let sut: IWalrusBucket;
-    let dateHelper : TimeHelper;
+  let container: IContainer;
+  let walrusBucketConstructor: WalrusBucketConstructor;
+  let taskConstructor: TaskConstructor;
+  let sut: IWalrusBucket;
+  let dateHelper: TimeHelper;
 
-    beforeEach(() => {
-        container = getContainer();
+  beforeEach(() => {
+    container = getContainer();
 
-        dateHelper = new TimeHelper();
-        dateHelper.registerWith(container);
+    dateHelper = new TimeHelper();
+    dateHelper.registerWith(container);
 
-        taskConstructor = container.build(ITask);
-        walrusBucketConstructor = container.build(IWalrusBucket);
-        sut = walrusBucketConstructor();
-    });
+    taskConstructor = container.build(ITask);
+    walrusBucketConstructor = container.build(IWalrusBucket);
+    sut = walrusBucketConstructor();
+  });
 
-    it('be registered with the container', () => {
-        expect(sut).to.be.instanceOf(IWalrusBucket);
-    });
+  it('be registered with the container', () => {
+    expect(sut).to.be.instanceOf(IWalrusBucket);
+  });
 
-    it('have no tasks when created', () =>{
-      expect(sut.getAllTasks()).to.have.lengthOf(0);
-    });
+  it('have no tasks when created', () => {
+    expect(sut.getAllTasks()).to.have.lengthOf(0);
+  });
 
-    it('allow for an existing task to be added', () => {
-      let task = taskConstructor('A task');
+  it('allow for an existing task to be added', () => {
+    let task = taskConstructor('A task');
 
-      sut.add(task);
-      let r = sut.getAllTasks();
+    sut.add(task);
+    let r = sut.getAllTasks();
 
-      expect(r).to.have.lengthOf(1);
-      expect(r[0]).to.deep.equal(task);
-    });
+    expect(r).to.have.lengthOf(1);
+    expect(r[0]).to.deep.equal(task);
+  });
+
+  it('allow for a new task to be added', () => {
+    dateHelper.resetAfter(() =>
+      sut.addNew('A new task', 'Small')
+    );
+
+    const task = taskConstructor('A new task', 'Small');
+
+    let r = sut.getAllTasks();
+
+    expect(r).to.have.lengthOf(1);
+    expect(r[0].name).to.deep.equal(task.name);
+    expect(r[0].size).to.deep.equal(task.size);
+    expect(r[0].states).to.deep.equal(task.states);
+  });
+
+  it('allow for a new task to be added without size', () => {
+    dateHelper.resetAfter(() =>
+      sut.addNew('A new task')
+    );
+
+    const task = taskConstructor('A new task');
+
+    let r = sut.getAllTasks();
+
+    expect(r).to.have.lengthOf(1);
+    expect(r[0].name).to.deep.equal(task.name);
+    expect(r[0].size).to.deep.equal(task.size);
+    expect(r[0].states).to.deep.equal(task.states);
+  });
 });
