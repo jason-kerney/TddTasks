ed34eabca90aa3ee58734e32dba6ea2b78fc8b41
Jason Kerney <robert.kerney@edf-re.com>
2022-05-13 11:18:18 -0700

    R hid members behind properties

--- app/stateChange.ts
+++ app/stateChange.ts
@@ -1,18 +1,18 @@
 import { Activity, none, None } from "./generalTypes";
 import { Factory, IContainer } from "./container"
 
-export type StateChangeConstructor = (stateName: string, activity: Activity, activityDescriptor: string | None, previous? : IStateChange) => IStateChange
+export type StateChangeConstructor = (stateName: string, activity: Activity, activityDescriptor: string | None, previous?: IStateChange) => IStateChange
 
 export abstract class IStateChange {
-  abstract readonly stateName: string;
-  abstract readonly date: Date;
-  abstract readonly activity: Activity;
-  abstract readonly activityDescriptor: string | None;
-  abstract readonly previous: IStateChange | None;
+  abstract get stateName(): string;
+  abstract get date(): Date;
+  abstract get activity(): Activity;
+  abstract get activityDescriptor(): string | None;
+  abstract get previous(): IStateChange | None;
 
-  getFirst() : IStateChange {
+  getFirst(): IStateChange {
     let r = this as IStateChange
-    while(r.previous !== none) {
+    while (r.previous !== none) {
       r = r.previous;
     }
 
@@ -26,7 +26,7 @@ export abstract class IStateChange {
   count(): number {
     let r = 1;
     let p = this.previous;
-    if(p !== none) {
+    if (p !== none) {
       r += p.count();
     }
 
@@ -35,25 +35,44 @@ export abstract class IStateChange {
 }
 
 class StateChange extends IStateChange {
-  readonly stateName: string;
-  readonly date: Date;
-  readonly activity: Activity;
-  readonly activityDescriptor: string | None;
-  readonly previous: IStateChange | "none";
+  readonly iStateName: string;
+  get stateName(): string {
+    return this.iStateName;
+  }
 
-  constructor(stateName: string, activity: Activity, activityDescriptor: string | None, date: Date, previous : IStateChange | None) {
+  readonly iDate: Date;
+  get date(): Date {
+    return this.iDate;
+  }
+
+  readonly iActivity: Activity;
+  get activity(): Activity {
+    return this.iActivity;
+  }
+
+  readonly iActivityDescriptor: string | None;
+  get activityDescriptor(): string {
+    return this.iActivityDescriptor;
+  }
+
+  readonly iPrevious: IStateChange | "none";
+  get previous(): IStateChange | "none" {
+    return this.iPrevious;
+  }
+
+  constructor(stateName: string, activity: Activity, activityDescriptor: string | None, date: Date, previous: IStateChange | None) {
     super();
-    this.stateName = stateName;
-    this.date = new Date(date);
-    this.activity = activity;
-    this.activityDescriptor = activityDescriptor;
-    this.previous = previous;
+    this.iStateName = stateName;
+    this.iDate = new Date(date);
+    this.iActivity = activity;
+    this.iActivityDescriptor = activityDescriptor;
+    this.iPrevious = previous;
   }
 }
 
 export const stateChangeBuilder =
-  function(factory: IContainer){
-    const ctor: StateChangeConstructor = (name: string, activity: Activity, activityDescriptor: string | None, previous : IStateChange | None = none) => {
+  function (factory: IContainer) {
+    const ctor: StateChangeConstructor = (name: string, activity: Activity, activityDescriptor: string | None, previous: IStateChange | None = none) => {
       const date = factory.build(Date)();
 
       return new StateChange(name, activity, activityDescriptor, date, previous);
